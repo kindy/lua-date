cf4af9bdc101b62a7b802e0568a1a2585c4a335e
shrimp (郭颖) <shrimp0597@gmail.com>
2011-11-24 15:30:02 +0800

    update test.lua

--- test.lua
+++ test.lua
@@ -1,4 +1,12 @@
 local date = require("lua-date")
 
-local isdate = date.days_diff("2011-12-12", "2012-01-11")
-print(isdate)
+-- return interval days between two dates, result is 31
+local days = date.days_diff("2011-12-12", "2012-01-11")
+
+-- return dates of table between two given dates
+local date_table = date.days_between("2011-12-12", "2011-12-14")
+
+-- date_table is {"2011-12-12", "2011-12-13", "2011-12-14"}
+for k, v in ipairs(date_table) do
+    print(v)
+end
